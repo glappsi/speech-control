65455ff8e8fc4901823c0fc21dbc0d87cab9f869
Florian Glappa <fg@fino.digital>
2019-10-09 22:20:21 +0200

    exclude rxjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,11 +12,16 @@ const libraryName = 'speech-control'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true
+    },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [...Object.keys(pkg.peerDependencies || {})],
+  external: ['rxjs', 'rxjs/operators'],
   watch: {
     include: 'src/**'
   },
